377dd199cb459b3fba987954e55271e6bc501573
Kirill Fomichev <fanatid@ya.ru>
2015-01-04 23:26:51 +0300

    new cc-wallet-core add CInputsOperationalTx.getRequiredFee small style changes

--- service.js
+++ service.js
@@ -1,10 +1,10 @@
 var express = require('express');
 
-var jsonBody = require("body/json");
-var sendJson = require("send-data/json");
+var jsonBody = require('body/json');
+var sendJson = require('send-data/json');
 var stringify = require('json-stable-stringify');
-var SHA256 = require("crypto-js/sha256");
-var server_process = require("./src/server_process");
+var SHA256 = require('crypto-js/sha256');
+var server_process = require('./src/server_process');
 var cors = require('cors');
 var logger = require('morgan');
 
@@ -14,56 +14,61 @@ var app = express();
 var cors_options = {origin: true, credentials: true};
 app.use(logger());
 app.use(cors(cors_options));
-app.options('*', cors(cors_options)); 
+app.options('*', cors(cors_options));
 
 var cwpp_api = express.Router();
 
 var pay_reqs = {};
 
 cwpp_api.post('/new-request', function (req, res) {
-    console.log('new-request');
-    jsonBody(req, function (err, body) {
-        var hash = SHA256(stringify(body)).toString();
-        pay_reqs[hash] = body;
-        sendJson(req, res, {"hash": hash});
-    });
+  console.log('new-request');
+  jsonBody(req, function (err, body) {
+    var hash = SHA256(stringify(body)).toString();
+    pay_reqs[hash] = body;
+    sendJson(req, res, {"hash": hash});
+  });
 });
 
 cwpp_api.get('/:rq_hash', function (req, res) {
-    if (pay_reqs[req.params.rq_hash])
-        res.json(pay_reqs[req.params.rq_hash]);
-    else
-        res.status(404).json({error: 'requrest not found'});
+  if (pay_reqs[req.params.rq_hash]) {
+    res.json(pay_reqs[req.params.rq_hash]);
+
+  } else {
+    res.status(404).json({error: 'requrest not found'});
+
+  }
 });
 
 function process_request(rq, body, cb) {
-    server_process.process_request(rq, body, cb);
+  server_process.process_request(rq, body, cb);
 }
 
 cwpp_api.post('/process/:rq_hash', function (req, res) {
-    if (pay_reqs[req.params.rq_hash])
-        jsonBody(req, function (err, body) {
-            if (err)
-                res.status(400).json({error: 'JSON required'});
-            else {
-                process_request(pay_reqs[req.params.rq_hash], body,
-                function (err, res_body) {
-                    if (err) {
-                        res.status(500).json({error: err.toString()});
-                        console.log(err.stack);
-                    }                        
-                    else
-                        res.json(res_body);
-                });
-            }
-        });
-    else
-        res.status(404).json({error: 'requrest not found'});
+  if (typeof pay_reqs[req.params.rq_hash] === 'undefined') {
+    return res.status(404).json({error: 'requrest not found'});
+  }
+
+  jsonBody(req, function (error, body) {
+    if (error) {
+      return res.status(400).json({error: 'JSON required'});
+    }
+
+    process_request(pay_reqs[req.params.rq_hash], body, function (error, res_body) {
+      if (error) {
+        res.status(500).json({error: err.toString()});
+        console.log(err.stack);
+
+      } else {
+        res.json(res_body);
+
+      }
+    });
+  });
 });
 
 app.use('/cwpp', cwpp_api);
 
 
 var server = app.listen(4242, function () {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
--- src/server_process.js
+++ src/server_process.js
@@ -13,187 +13,224 @@ var BIP39 = require('bip39');
 var wallet = null;
 var seed = null;
 
+
 function addr_to_script_hex(addr) {
-    var script = bitcoin.Address.fromBase58Check(addr).toOutputScript();
-    return script.toHex();
+  var script = bitcoin.Address.fromBase58Check(addr).toOutputScript();
+  return script.toHex();
 }
 
 function initialize_wallet() {
-    var systemAssetDefinitions = [{
-        monikers: ['gold'],
-        colorDescs: ['epobc:b95323a763fa507110a89ab857af8e949810cf1e67e91104cd64222a04ccd0bb:0:180679']
-    }, {
-        colorDescs: ["epobc:0261b29b587020eeca15f831a5290a9d81038851da4365689be04e588ce58c66:0:303510"],
-        monikers: ["euro"], 
-        unit: 100
-    }];
-
-    wallet = new WalletCore.Wallet({
-            testnet: true, 
-            blockchain: 'NaiveBlockchain',
-            systemAssetDefinitions: systemAssetDefinitions
-    });
-    
-    var mnemonic = 'provide rail journey neither script nasty fetch south seat obvious army two';
-    var password = '';
-    seed = BIP39.mnemonicToSeedHex(mnemonic, password);
-    if (!wallet.isInitialized()) {
-        wallet.initialize(seed);
+  var systemAssetDefinitions = [
+    {
+      monikers: ['gold'],
+      colorDescs: ['epobc:b95323a763fa507110a89ab857af8e949810cf1e67e91104cd64222a04ccd0bb:0:180679']
+    },
+    {
+      colorDescs: ['epobc:0261b29b587020eeca15f831a5290a9d81038851da4365689be04e588ce58c66:0:303510'],
+      monikers: ['euro'],
+      unit: 100
     }
-    console.log("My Bitcoin address:");
-    console.log(wallet.getSomeAddress(wallet.adManager.getByMoniker('bitcoin'), false));
-    wallet.on('error', function (err) { console.log(err);});
-    wallet.subscribeAndSyncAllAddresses(function () {});
+  ];
+
+  wallet = new WalletCore.Wallet({
+    testnet: true,
+    blockchain: 'NaiveBlockchain',
+    systemAssetDefinitions: systemAssetDefinitions
+  });
+
+  var mnemonic = 'provide rail journey neither script nasty fetch south seat obvious army two';
+  var password = '';
+  seed = BIP39.mnemonicToSeedHex(mnemonic, password);
+  if (!wallet.isInitialized()) {
+    wallet.initialize(seed);
+  }
+  console.log('My Bitcoin address:');
+  console.log(wallet.getSomeAddress(wallet.adManager.getByMoniker('bitcoin'), false));
+  wallet.on('error', function (error) { console.log(error); });
+  wallet.subscribeAndSyncAllAddresses(function () {});
 }
 
 function get_wallet() {
-    if (!wallet) initialize_wallet();
-    return wallet;
+  if (!wallet) {
+    initialize_wallet();
+  }
+
+  return wallet;
 }
 
 function get_seed() {
-    if (!seed) initialize_wallet();
-    return seed;
+  if (!seed) {
+    initialize_wallet();
+  }
+
+  return seed;
 }
 
 
 function check_protocol(msg) {
-    if (msg.protocol != 'cwpp/0.0')
-        throw "protocol not supported";
-    return true;
+  if (msg.protocol != 'cwpp/0.0') {
+    throw new Error('protocol not supported');
+  }
+  return true;
 }
 
 function cinputs_colordef(payreq, procreq) {
-    var colorDesc = payreq.colorDesc;
-    return get_wallet().cdManager.resolveByDesc(colorDesc, false);
+  var colorDesc = payreq.colorDesc;
+  return get_wallet().cdManager.resolveByDesc(colorDesc, true);
 }
 
 function CInputsOperationalTx(wallet, colordef) {
-    this.wallet = wallet;
-    this.colordef = colordef;
-    this.targets = [];
+  this.wallet = wallet;
+  this.colordef = colordef;
+  this.targets = [];
 }
 
 inherits(CInputsOperationalTx, OperationalTx);
 
 CInputsOperationalTx.prototype.getChangeAddress = function (colordef) {
-    if (colordef.getColorType() !== 'uncolored')
-        throw new Error('colored change not supported');
-    return OperationalTx.prototype.getChangeAddress.call(this, colordef);
-}
+  if (colordef.getColorType() !== 'uncolored') {
+    throw new Error('colored change not supported');
+  }
+
+  return OperationalTx.prototype.getChangeAddress.call(this, colordef);
+};
 
 
 CInputsOperationalTx.prototype.addColoredInputs = function (cinputs) {
-    var wallet = get_wallet();
-    this.ccoins = cinputs.map(function (rawCoin) {
-        return new Coin(wallet.coinManager, rawCoin);
-    });
+  // dirty hack
+  var coinManager = get_wallet().getStateManager()._currentState.getCoinManager();
+  this.ccoins = cinputs.map(function (rawCoin) {
+    return new Coin(coinManager, rawCoin);
+  });
 };
 
+CInputsOperationalTx.prototype.getRequiredFee = function (txsize) {
+  return new ColorValue(new cclib.UncoloredColorDefinition(), 0);
+};
 
-CInputsOperationalTx.prototype.selectCoins = function (colorValue, 
-    feeEstimator, cb) {
-    var self = this;
-    if (!colorValue.isUncolored()) {
-        (new CoinList(this.ccoins)).getTotalValue(function (err, totalValues) {
-            if (err) cb(err);
-            else {
-                if (totalValues.length !== 1)
-                    cb(new Error('provided coins have ambiguous colorvalue'));
-                else if (totalValues[0].getColorId() !== self.colordef.getColorId())
-                    cb(new Error('provided coins are of a wrong color'));
-                else
-                    cb(err, self.ccoins, totalValues[0]);
-            }
-        });
-    } else {
-        OperationalTx.prototype.selectCoins.call(
-            this, colorValue, feeEstimator, cb);
+CInputsOperationalTx.prototype.selectCoins = function (colorValue, feeEstimator, cb) {
+  var self = this;
+  if (colorValue.isUncolored()) {
+    return OperationalTx.prototype.selectCoins.call(this, colorValue, feeEstimator, cb);
+  }
+
+  var coinList = new CoinList(this.ccoins);
+  coinList.getTotalValue(function (error, totalValues) {
+    if (error) {
+      return cb(error);
     }
-};
 
+    if (totalValues.length !== 1) {
+      return cb(new Error('provided coins have ambiguous colorvalue'));
+    }
+
+    if (totalValues[0].getColorId() !== self.colordef.getColorId()) {
+      return cb(new Error('provided coins are of a wrong color'));
+    }
+
+    cb(error, self.ccoins, totalValues[0]);
+  });
+};
 
 function cinputs_operational_txs(payreq, procreq) {
-    var colordef = cinputs_colordef(payreq, procreq);
-    var op_txs = new CInputsOperationalTx(get_wallet(), colordef);
-    op_txs.addTarget(new ColorTarget(addr_to_script_hex(payreq.address),
-                                     new ColorValue(colordef, payreq.value)));
-    if (procreq.change)
-        op_txs.addTarget(
-            new ColorTarget(addr_to_script_hex(procreq.change.address),
-                    new ColorValue(colordef, procreq.change.value)));
-    op_txs.addColoredInputs(procreq.cinputs);    
-    return op_txs;    
+  var colordef = cinputs_colordef(payreq, procreq);
+  function createColorTarget(address, value) {
+    return new ColorTarget(addr_to_script_hex(address), new ColorValue(colordef, value));
+  }
+
+  var op_txs = new CInputsOperationalTx(get_wallet(), colordef);
+  op_txs.addColoredInputs(procreq.cinputs);
+  op_txs.addTarget(createColorTarget(payreq.address, payreq.value));
+  if (procreq.change) {
+    op_txs.addTarget(createColorTarget(procreq.change.address, procreq.change.value));
+  }
+
+  return op_txs;
 }
 
 function process_cinputs_1(payreq, procreq, cb) {
-    var colordef = cinputs_colordef(payreq, procreq);
-    var optxs = cinputs_operational_txs(payreq, procreq);
-    colordef.constructor.makeComposedTx(optxs, function (err, ctx) {
-        if (err) return cb(err);
-        else return get_wallet().transformTx(ctx, 'raw', null, function (err, tx) {
-            if (err) return cb(err);
-            return cb(null, {"protocol": "cwpp/0.0",
-                             "tx_data": tx.toHex(true)});
-        });
+  var colordef = cinputs_colordef(payreq, procreq);
+  var optxs = cinputs_operational_txs(payreq, procreq);
+  colordef.constructor.makeComposedTx(optxs, function (error, ctx) {
+    if (error) {
+      return cb(error);
+    }
+
+    get_wallet().transformTx(ctx, 'raw', {}, function (error, tx) {
+      if (error) {
+        return cb(error);
+      }
+
+      cb(null, {'protocol': 'cwpp/0.0', 'tx_data': tx.toHex(true)});
     });
+  });
 }
 
 function cinputs_check_tx(payreq, procreq, rtx, cb) {
-    var wallet = get_wallet();
-    var bs = wallet.getBlockchain();
-    var colordef = cinputs_colordef(payreq, procreq);
-    wallet.getColorData().getColorValuesForTx(
-        rtx.toTransaction(true), colordef, bs.getTx.bind(bs), 
-        function (err, colorvalues) {
-            // TODO
-            if (err) cb(err);
-            else cb(null);
-        });
+  var tx = rtx.toTransaction(true);
+  var colordef = cinputs_colordef(payreq, procreq);
+  var getTxFn = get_wallet().getBlockchain().getTxFn();
+  get_wallet().getColorData().getTxColorValues(tx, colordef, getTxFn, function (error) {
+    // @todo
+    cb(error);
+  });
 }
 
 function process_cinputs_2(payreq, procreq, cb) {
-    var tx = RawTx.fromHex(procreq.tx);
-    cinputs_check_tx(payreq, procreq, tx, function (err) {
-        if (err) cb(err);
-        else 
-        get_wallet().transformTx(tx, 'signed', seed, function (err, stx) {
-            if (err) cb(err);
-            else cb(null, {protocol: "cwpp/0.0",
-                           "tx_data": stx.toHex()});
-        });
+  var tx = RawTx.fromHex(procreq.tx);
+  cinputs_check_tx(payreq, procreq, tx, function (error) {
+    if (error) {
+      return cb(error);
+    }
+
+    get_wallet().transformTx(tx, 'signed', {seedHex: seed}, function (error, stx) {
+      if (error) {
+        return cb(error);
+      }
+
+      cb(null, {protocol: 'cwpp/0.0', 'tx_data': stx.toHex()});
     });
+  });
 }
 
 
 function process_cinputs(payreq, procreq, cb) {
-    if (payreq.messageType != 'PaymentRequest')
-        throw "PaymentRequest expected";
-    if (!(payreq.acceptedMethods && 
-          payreq.acceptedMethods['cinputs']))
-        throw "PaymentRequest doesn't support cinputs";
-    if (procreq.stage == 1)
-        return process_cinputs_1(payreq, procreq, cb);
-    else if (procreq.stage == 2)
-        return process_cinputs_2(payreq, procreq, cb);
-    else
-        throw 'invalid stage for cinputs';
+  if (payreq.messageType != 'PaymentRequest') {
+    throw new Error('PaymentRequest expected');
+  }
+
+  if (!(payreq.acceptedMethods && payreq.acceptedMethods.cinputs)) {
+    throw new Error('PaymentRequest doesn\'t support cinputs');
+  }
+
+  if (procreq.stage == 1) {
+    return process_cinputs_1(payreq, procreq, cb);
+  }
+
+  if (procreq.stage == 2) {
+    return process_cinputs_2(payreq, procreq, cb);
+  }
+
+  throw new Error('invalid stage for cinputs');
 }
 
 function process_request(payreq, procreq, cb) {
-    try {
-        check_protocol(payreq);
-        check_protocol(procreq);
-        if (procreq.messageType != 'ProcessRequest')
-            throw "ProcessRequest expected";
-        if (procreq.method == 'cinputs')
-            return process_cinputs(payreq, procreq, cb);
-        else
-            throw "method not supported";
-    } catch (x) {
-        cb(x, null);
+  try {
+    check_protocol(payreq);
+    check_protocol(procreq);
+
+    if (procreq.messageType != 'ProcessRequest') {
+      throw new Error('ProcessRequest expected');
     }
-    
+
+    if (procreq.method == 'cinputs') {
+      return process_cinputs(payreq, procreq, cb);
+    }
+
+    throw new Error('method not supported');
+  } catch (error) {
+    cb(error, null);
+  }
 }
 
 module.exports.process_request = process_request;
